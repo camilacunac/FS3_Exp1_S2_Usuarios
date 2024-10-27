7245b8d4d3b892137f6d6f1a8bd42b553a725a3c
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-10-27 15:14:28 -0300

    add repository

---
+++ src/main/java/com/example/usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.usuarios.repository;
+
+import com.example.usuarios.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByCorreo(String correo);
+}
